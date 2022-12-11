19e9b4e0ccf523e4ab3ce90fffe7bf5b25d8a915
Aggelos Katsaliros <aggeloskatsaliros@hotmail.com>
2022-12-11 16:02:14 +0200

    Implementd CRUD operations in main

--- src/main/java/com/practice/Main.java
+++ src/main/java/com/practice/Main.java
@@ -2,14 +2,13 @@ package com.practice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @SpringBootApplication
 @RestController
-//@RequestMapping("api/v1/customers")
+@RequestMapping("api/v1/customers")
 public class Main {
 
     private final CustomerRepository customerRepository;
@@ -22,10 +21,39 @@ public class Main {
         SpringApplication.run(Main.class, args);
     }
 
-    @GetMapping("/api/v1/customers")
+    @GetMapping
     public List<Customer> getCustomers(){
         return customerRepository.findAll();
     }
+
+    record NewCustomerRequest(
+            String name,
+            String email,
+            Integer age
+    ){}
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerRequest request){
+        Customer customer = new Customer();
+        customer.setName(request.name());
+        customer.setEmail(request.email());
+        customer.setAge(request.age());
+        customerRepository.save(customer);
+    }
+    @DeleteMapping("{customerId}")
+    public void deleteCustomer(@PathVariable("customerId") Integer id){
+        customerRepository.deleteById(id);
+    }
+
+    @PutMapping("{customerId}")
+    public void updateCustomer(@PathVariable("customerId") Integer id,
+                               @RequestBody NewCustomerRequest request){
+        Customer customer = customerRepository.findById(id);
+        customerRepository.findById(id).setName(request.name());
+        customer.setEmail(request.email());
+        customer.setAge(request.age());
+        customerRepository.save(customer);
+    }
+
     @GetMapping("/greet")
     public GreetResponse greet(){
          GreetResponse response = new GreetResponse("Hello",
